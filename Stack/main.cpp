e7c52582c53133d690790fb0efb1f9a961634590
first rays of the morning sun <164125845+TurningIntoWind@users.noreply.github.com>
2025-07-23 10:27:35 +0800

    Update main.cpp

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "evaluator.h"
 
+int main()
+{
+	std::string exp;
+
+	for (int i = 0; i < 3; i++)
+	{
+		std::cin >> exp;
+		std::cout << Eval(exp) << std::endl;
+	}
+
+	return 0;
+}
+
+// 35+
+// 35+2*
+// 123 * +3 - 2 /
